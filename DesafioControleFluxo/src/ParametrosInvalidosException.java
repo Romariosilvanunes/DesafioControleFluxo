d7e0d1bec22c7ab972ce4e04c2b1a94525398dca
romariosilva <1994romariosilvanunes@gmail.com>
2023-10-04 15:35:28 -0300

    Finalizando desafio

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+class ParametrosInvalidosException extends Exception{
+    ParametrosInvalidosException(){
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+    }
+   
+}
+
+
+
